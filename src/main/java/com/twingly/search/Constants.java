6929884480d8ce27a44d059b0b29c3229c54d244
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2016-03-04 17:44:04 +0200

    resolve twingly/twingly-search-api-java#10
    added support for getting version for gradle and Java from one version.properties file

--- src/main/java/com/twingly/search/Constants.java
+++ src/main/java/com/twingly/search/Constants.java
@@ -1,20 +1,35 @@
 package com.twingly.search;
 
+import java.io.FileInputStream;
+import java.net.URL;
+import java.util.Properties;
+
 /**
  * API-wide constants
  */
 public final class Constants {
-    /**
-     * The constant VERSION.
-     */
-    public static final String VERSION = "1.0.1";
+
     /**
      * The constant DATE_FORMAT.
      */
     public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
-
     /**
      * The constant TWINGLY_API_KEY_PROPERTY
      */
     public static final String TWINGLY_API_KEY_PROPERTY = "TWINGLY_SEARCH_KEY";
+    private static final Properties VERSION_PROPERTIES = new Properties();
+    /**
+     * The constant VERSION.
+     */
+    public static final String VERSION = VERSION_PROPERTIES.getProperty("version");
+
+    static {
+        URL resource = Constants.class.getClassLoader().getResource("version.properties");
+        assert resource != null;
+        try (FileInputStream fis = new FileInputStream(resource.getFile())) {
+            VERSION_PROPERTIES.load(fis);
+        } catch (Exception e) {
+            throw new RuntimeException("version.properties was not found or cannot be read", e);
+        }
+    }
 }
