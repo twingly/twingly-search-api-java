a73f0626f1eaf0ac0c1909899e89fc900ba582b0
Robin Wallin <robin@twingly.com>
2016-04-25 14:21:13 +0200

    Add test for empty result set

--- src/test/groovy/com/twingly/search/client/UrlConnectionClientSpockTest.groovy
+++ src/test/groovy/com/twingly/search/client/UrlConnectionClientSpockTest.groovy
@@ -108,6 +108,21 @@ class UrlConnectionClientSpockTest extends Specification {
         result.posts[0].contentType == ContentType.BLOG
     }
 
+    def "should parse valid empty result"() {
+        given:
+        def filepath = packagePath.resolve("valid_empty_result.xml")
+        def is = this.getClass().getClassLoader().getResourceAsStream(filepath.toString())
+        when:
+        def result = is.withReader("UTF-8", {
+            r -> return client.unmarshalXmlForResult(r)
+        })
+        then:
+        result.numberOfMatchesReturned == 0
+        result.numberOfMatchesTotal == 0
+        result.secondsElapsed == 0.213d
+        result.posts.size() == 0
+    }
+
     def "should throw exception for undefined error result"() {
         given:
         def filepath = packagePath.resolve("undefined_error_result.xml")
