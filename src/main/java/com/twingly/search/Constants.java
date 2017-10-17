087a2185ff9c2c3816f08b89a803c21b29cb92f1
Robin Wallin <robin@twingly.com>
2017-10-17 10:11:19 +0200

    Include 'T' in timestamp queries

--- src/main/java/com/twingly/search/Constants.java
+++ src/main/java/com/twingly/search/Constants.java
@@ -12,7 +12,7 @@ public final class Constants {
     /**
      * The constant DATE_FORMAT.
      */
-    public static final String QUERY_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String QUERY_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
     public static final String POST_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssX";
 
     /**
